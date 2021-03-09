ba20639ab8b5f09b904cc157f4f350c7424dffb3
MaxLIE2018 <liebsch@pm.me>
2021-03-09 11:16:41 +0100

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-var test = 2;
+let test = 2
+let test2 = 2
+
+console.log(test + test2)
+
+//var or let variable assignment
+//var is global and reachable throughout the file
+//efficiency is low, uses more ram then let
